76dfcdbe66ca34f16e4310494222fbd2fee13704
leandrojordao <leandro.jordao@wisebyte.com.br>
2025-12-01 15:27:57 -0300

    mudando nome do script

--- scp.sh
+++ scp.sh
@@ -5,7 +5,7 @@ mvn clean install -Dmaven.test.skip=true
 
 # Empacota a aplicação
 cd target
-zip -r app.zip quarkus-app
+zip -r samarco.zip quarkus-app
 
 # Envia para o servidor miadev
 scp samarco.zip miadev:/tmp
